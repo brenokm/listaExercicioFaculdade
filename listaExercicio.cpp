c48982ea007c699a328f1930c3f19568b00837ae
Breno Matos <127207889+brenokm@users.noreply.github.com>
2025-10-30 21:32:08 -0300

    Create listaExercicio.cpp

---
+++ listaExercicio.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+using namespace std;
+int main(){
+//1
+/*
+int a;
+int b;
+
+cout<<"insira o primeiro numero"<<endl;
+cin>>a;
+cout<<"insira o segundo numero"<<endl;
+cin>>b;
+
+if(a>b){
+cout<<a<<endl;
+}
+else if(a==b){
+cout<<"Os numeros sao equivalentes"<<endl;
+}
+else{
+cout<<b<<endl;
+}
+*/
+
+
+
+
+
+
+
+
+//2
+/*int a;
+int b;
+
+cout<<"insira o primeiro numero"<<endl;
+cin>>a;
+cout<<"insira o segundo numero"<<endl;
+cin>>b;
+
+if(a>b){
+cout<<"o Primeiro numero eh maior"<<endl;
+}
+else if(a==b){
+cout<<"Os numeros sao equivalentes"<<endl;
+}
+else{
+cout<<"o Segundo numero eh maior"<<endl;
+}
+*/
+
+//3
+/*int a;
+
+
+cout<<"insira o numero"<<endl;
+cin>>a;
+
+if(a%2==0){
+cout<<"o numero eh par"<<endl;
+}
+
+else{
+cout<<"o numero eh impar"<<endl;
+}
+*/
+
+//4
+/*int a;
+cout<<"insira o  numero"<<endl;
+cin>>a;
+
+
+if(a>80){
+cout<<"o numero eh maior que 80"<<endl;
+}
+else if(a==40){
+cout<<"O numeros é igual a 40"<<endl;
+}
+else if(a<25){
+    cout<<"o numero é menor que 25"<<endl;
+
+}
+else {
+cout<<"o numero nao se encaixa nos requisitos"<<endl; 
+}
+*/
+
+//5
+/*string a;
+cout<<"insira a senha correta"<<endl;
+cin>>a;
+
+if(a=="batata frita"){
+  cout<<"senha correta"<<endl; 
+}
+else{
+cout<<"senha incorreta"<<endl; 
+}
+*/
+
+//4
+int a;
+int b;
+int c;
+
+cout<<"insira o primeiro numero"<<endl;
+cin>>a;
+cout<<"insira o segundo numero"<<endl;
+cin>>b;
+cout<<"insira o terceiro numero"<<endl;
+cin>>c;
+
+if((a<b)&&(b<c)){
+cout<<a<<b<<c<<endl;
+}
+else if((c<b)&&(b<)&&())
+
+
+}
